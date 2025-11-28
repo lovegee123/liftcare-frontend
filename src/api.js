91d6cdff338c55d107cab748d943e10e640b856d
lovegee123 <167515974+lovegee123@users.noreply.github.com>
2025-11-28 12:37:47 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,6 +1,6 @@
 import { useAuth } from "./auth";
 
-export const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
+export const API_BASE = import.meta.env.VITE_API_BASE || "liftcare-backend-production.up.railway.app";
 
 // hook ใช้ใน component
 export function useApi() {
